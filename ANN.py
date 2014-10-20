c823a476b265b46d27b221831be952a811fe3468
Tysonzero <danielspencersmith@gmail.com>
2014-10-20 12:46:09 -0600

    Create 2D list of Neurons in NeuronNetwork's init

--- ANN.py
+++ ANN.py
@@ -3,3 +3,10 @@ class Neuron:
 
 class NeuronNetwork:
     neurons = []
+
+    def __init__(self, rows, columns):
+        self.neurons = []
+        for row in xrange(rows):
+            self.neurons.append([])
+            for column in xrange(columns):
+                self.neurons[row].append(Neuron())
