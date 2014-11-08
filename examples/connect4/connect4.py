3b0ec8e6e0888895d2f159b696eaf45e5d1aad84
Tysonzero <danielspencersmith@gmail.com>
2014-11-07 19:56:46 -0700

    Create Connect4.move method

--- examples/connect4/connect4.py
+++ examples/connect4/connect4.py
@@ -2,6 +2,9 @@ class Connect4(object):
     def __init__(self):
         self.pieces = [[] for i in xrange(7)]
 
+    def move(self, player, row):
+        self.pieces[row % 7].append(player)
+
 
 def start():
     pass
