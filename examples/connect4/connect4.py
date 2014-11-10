f01a5a5ca17423956c700ba61ea3f08ada4aeeac
Tysonzero <danielspencersmith@gmail.com>
2014-11-09 23:13:56 -0700

    Make Connect4 class keep track of whose turn it is

--- examples/connect4/connect4.py
+++ examples/connect4/connect4.py
@@ -1,9 +1,11 @@
 class Connect4(object):
     def __init__(self):
         self.pieces = [[] for i in xrange(7)]
+        self.turn = 1
 
-    def move(self, player, row):
-        self.pieces[row % 7].append(player)
+    def move(self, row):
+        self.pieces[row % 7].append(self.turn)
+        self.turn = 3 - self.turn
 
     def __str__(self):
         output = ''
