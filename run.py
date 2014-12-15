574fba0650e4c68b7a31533207c26d8d57ed49c2
Tysonzero <danielspencersmith@gmail.com>
2014-12-15 18:47:15 -0500

    Check length of argv before accessing it

--- run.py
+++ run.py
@@ -4,5 +4,6 @@ from examples.connect4.connect4 import Connect4Network
 
 
 if __name__ == '__main__':
-    if argv[1] == 'connect4':
-        Connect4Network().play()
+    if len(argv) > 1:
+        if argv[1] == 'connect4':
+            Connect4Network().play()
