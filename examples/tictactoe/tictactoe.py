82a799dfe7541b640f31ff782c142a4b3e89ed42
Tysonzero <danielspencersmith@gmail.com>
2014-12-17 02:09:30 +0000

    Create __str__ method

--- examples/tictactoe/tictactoe.py
+++ examples/tictactoe/tictactoe.py
@@ -9,3 +9,10 @@ class TicTacToe(object):
                 self.pieces[i % 9] = self.turn
                 self.turn = 1 - self.turn
                 return
+
+    def __str__(self):
+        output = ''
+        for i, piece in enumerate(self.pieces):
+            output += '' if not i else '\n' if not i % 3 else ' '
+            output += 'X' if piece else 'O' if piece is not None else str(i)
+        return output
