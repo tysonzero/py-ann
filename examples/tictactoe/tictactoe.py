00073a9bba778c8ab4cd777314ae00d084ee9a62
Tysonzero <danielspencersmith@gmail.com>
2014-12-17 01:55:35 +0000

    Create move method

--- examples/tictactoe/tictactoe.py
+++ examples/tictactoe/tictactoe.py
@@ -2,3 +2,8 @@ class TicTacToe(object):
     def __init__(self):
         self.pieces = [None for _ in xrange(9)]
         self.turn = 0
+
+    def move(self, position):
+        for i in xrange(position, position + 9):
+            if self.pieces[i % 9] is None:
+                self.pieces[i % 9] = self.turn
