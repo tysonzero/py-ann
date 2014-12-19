5be5c36c5174d0f38c23919566cf3180235804be
Tysonzero <danielspencersmith@gmail.com>
2014-12-19 23:01:01 +0000

    Create TicTacToe check method

--- examples/tictactoe/tictactoe.py
+++ examples/tictactoe/tictactoe.py
@@ -3,6 +3,20 @@ class TicTacToe(object):
         self.pieces = [None for _ in xrange(9)]
         self.turn = 0
 
+    def check(self, position):
+        if len(set(self.pieces[3*(position/3):3*(position/3 + 1)])) == 1:
+            return self.pieces[position]
+        if len(set(self.pieces[position % 3::3])) == 1:
+            return self.pieces[position]
+        if not position % 4:
+            if len(set(self.pieces[0::4])) == 1:
+                return self.pieces[position]
+        if position == 4 or position % 4 == 2:
+            if len(set(self.pieces[2::2][0:3])) == 1:
+                return self.pieces[position]
+        if set(self.pieces) == set([0, 1]):
+            return 0.5
+
     def move(self, position):
         for i in xrange(position, position + 9):
             if self.pieces[i % 9] is None:
