e9e16991de6ff2434fa54d23a8b9b1e3879b7ca7
Tysonzero <danielspencersmith@gmail.com>
2014-12-21 18:05:03 -0700

    Add tictactoe to run.py

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 from sys import argv
 
 from examples.connect4.connect4 import Connect4Network
+from examples.tictactoe.tictactoe import TicTacToeNetwork
 
 
 if __name__ == '__main__':
     if len(argv) > 1:
         if argv[1] == 'connect4':
             Connect4Network().play()
+        if argv[1] == 'tictactoe':
+            TicTacToeNetwork().play()
