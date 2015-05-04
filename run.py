497b4e6c9ad4554c16c38c4698a81ede0a3211d8
Tysonzero <danielspencersmith@gmail.com>
2015-05-03 23:45:34 -0600

    List available commands if no arguments passed

--- run.py
+++ run.py
@@ -5,8 +5,13 @@ from examples.tictactoe.tictactoe import TicTacToeNetwork
 
 
 if __name__ == '__main__':
-    if len(argv) > 1:
-        if argv[1] == 'connect4':
-            Connect4Network().play()
-        if argv[1] == 'tictactoe':
-            TicTacToeNetwork().play()
+    if len(argv) == 1:
+        print
+        print 'Available Commands:'
+        print 'connect4'
+        print 'tictactoe'
+        print
+    elif argv[1] == 'connect4':
+        Connect4Network().play()
+    elif argv[1] == 'tictactoe':
+        TicTacToeNetwork().play()
